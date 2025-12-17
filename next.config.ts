a2cd39d4ebbe660a65a404e490755fa0c6f55c40
ZONENOP\thach <thachhuynhit.ut@gmail.com>
2025-12-17 20:12:46 +0700

    êdf

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    // This is not recommended unless you already have ESLint configured to run
-    // in a separate part of your workflow (for example, in CI or a pre-commit hook).
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
